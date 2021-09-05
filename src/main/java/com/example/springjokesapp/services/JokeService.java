5cb7690720006171ca7f24751b90e0c79e000f1c
Pulkit Aggarwal <pulkit.aggarwal@upgrad.com>
2021-09-05 15:30:01 +0530

    Create Jokes Service. Closes #3

---
+++ src/main/java/com/example/springjokesapp/services/JokeService.java
@@ -0,0 +1,10 @@
+package com.example.springjokesapp.services;
+
+/**
+ * @author <a href="pulkit.aggarwal@upgrad.com">Pulkit Aggarwal</a>
+ * @version 1.0
+ * @since 05/09/21
+ */
+public interface JokeService {
+    String getJoke();
+}
---
+++ src/main/java/com/example/springjokesapp/services/JokeServiceImpl.java
@@ -0,0 +1,24 @@
+package com.example.springjokesapp.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author <a href="pulkit.aggarwal@upgrad.com">Pulkit Aggarwal</a>
+ * @version 1.0
+ * @since 05/09/21
+ */
+@Service
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
